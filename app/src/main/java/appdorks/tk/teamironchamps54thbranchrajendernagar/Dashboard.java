80d83c5d297ab3d95d21966bdc505453e7d4252a
blaze <developer.appdorks@gmail.com>
2018-06-05 12:13:42 +0530

    add cameraButton and imageView to add form layout

--- app/src/main/java/appdorks/tk/teamironchamps54thbranchrajendernagar/Dashboard.java
+++ app/src/main/java/appdorks/tk/teamironchamps54thbranchrajendernagar/Dashboard.java
@@ -1,5 +1,6 @@
 package appdorks.tk.teamironchamps54thbranchrajendernagar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,9 +20,11 @@ public class Dashboard extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
+                startActivity(new Intent(getApplicationContext(), AddMember.class));
             }
         });
     }
