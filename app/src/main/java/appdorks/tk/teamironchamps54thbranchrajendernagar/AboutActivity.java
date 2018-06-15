05764a6fb0178c71cf881d4963cafb23e2eb137b
dbc2201 <divyanshb22@gmail.com>
2018-06-15 20:49:49 +0530

    add URL intent to imageViewAppDorks

--- app/src/main/java/appdorks/tk/teamironchamps54thbranchrajendernagar/AboutActivity.java
+++ app/src/main/java/appdorks/tk/teamironchamps54thbranchrajendernagar/AboutActivity.java
@@ -1,14 +1,19 @@
 package appdorks.tk.teamironchamps54thbranchrajendernagar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class AboutActivity extends AppCompatActivity
 {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +24,20 @@ public class AboutActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageView = findViewById(R.id.imageViewAppDorks);
+        imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String url = "http://www.appdorks.tk";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
     }
 
 }
